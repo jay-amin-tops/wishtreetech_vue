5a58864161984960462da1bc39787b3dff91c035
jay-amin-tops <jay.amin.tops@gmail.com>
2023-01-20 12:39:11 +0530

    vue

---
+++ vue/topics/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloW msg="Welcome to Your Vue.js App"/>
+  <HelloW msg="Welcome to diff Vue.js App"/>
+  <HelloW msg="Welcome to values Vue.js App"/>
+  <HelloW msg="Welcome to test Vue.js App"/>
+  <HelloW msg="Welcome to same Vue.js App"/>
+</template>
+
+<script>
+import HelloW from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloW
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
