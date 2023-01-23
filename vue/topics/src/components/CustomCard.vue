d79cf9d4926f5c79602be13ec19f37e4d8a5bc87
jay-amin-tops <jay.amin.tops@gmail.com>
2023-01-23 11:45:34 +0530

    vue

---
+++ vue/topics/src/components/CustomCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <div class="">Box 1</div>
+    <div class="">Box 2</div>
+    <div class="">Box 3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomCard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+</style>
