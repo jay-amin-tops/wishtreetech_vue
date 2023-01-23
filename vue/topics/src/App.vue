2fe93920d7cc436397bddaa0cc74f2613fe6fb3f
jay-amin-tops <jay.amin.tops@gmail.com>
2023-01-23 12:52:49 +0530

    vue card

--- vue/topics/src/App.vue
+++ vue/topics/src/App.vue
@@ -1,30 +1,67 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloW msg="Welcome to Your Vue.js App"/>
-  <HelloW msg="Welcome to diff Vue.js App"/>
-  <HelloW msg="Welcome to values Vue.js App"/>
-  <HelloW msg="Welcome to test Vue.js App"/>
-  <HelloW msg="Welcome to same Vue.js App"/>
+  <HelloW/>
+  <div class="container">
+    <div class="row"> 
+      <div class="col">
+        <CustomCard cardtitle="Believing is seeing"/>
+      </div>
+      <div class="col">
+        <CustomCard cardtitle="Believing is seeing"/>
+      </div>
+      <div class="col">
+        <CustomCard cardtitle="Believing is seeing"/>
+      </div>
+      <div class="col">
+        <CustomCard cardtitle="Believing is seeing"/>
+      </div>
+      <div class="col">
+        <CustomCard cardtitle="Believing is seeing"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import HelloW from './components/HelloWorld.vue'
+import CustomCard from './components/CustomCard'
 
 export default {
   name: 'App',
   components: {
-    HelloW
+    HelloW,
+    CustomCard
   }
 }
 </script>
 
-<style>
+
+<style scoped>
+.container{
+  width:1100px;
+  margin:0 auto;
+}
+.d-flex{
+  display:flex;
+}
+.col{
+  width:22%;
+  margin:10px;
+  /* display:inline; */
+  /* display:inline-block; */
+  /* border:1px solid; */
+  /* padding:50px; */
+}
+.row{
+  display:flex;
+  flex-wrap: wrap;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  /* text-align: center; */
   margin-top: 60px;
 }
 </style>
